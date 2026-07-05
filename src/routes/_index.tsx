f686ad50fbd7282dc7eb57983342c32bc32902f0
Mauricio González Prendas <me@maugp.com>
2026-07-04 21:44:42 -0600

    feat(routes): temporarily redirect index to schedule

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -1,3 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/_index")({});
+export const Route = createFileRoute("/_index")({
+  beforeLoad: () => {
+    throw redirect({
+      to: "/schedule",
+    });
+  },
+});
